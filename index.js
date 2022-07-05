1b0b5d14402c95ba55cbbc2b5eec0aebb838ea9e
Leo Volwahsen <volwahsenl@gmail.com>
2022-07-05 14:34:43 +0200

    warscheinlichkeit

---
+++ index.js
@@ -0,0 +1,8 @@
+//1. Entscheidungshilfe
+// Ausgabe soll entwerder Ja! oder Nein! sein
+let randomNumber = Math.floor(Math.random() * 2) + 1;
+if (randomNumber == 1) {
+    console.log("Ja!");
+} else {
+    console.log("Nein!");
+}
